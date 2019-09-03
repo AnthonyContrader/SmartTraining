6961f41e22b8ae561472417165441a0840357d64
Emilia <emiliaesposito-ee@libero.it>
2019-09-03 12:25:20 +0200

    StudentService

---
+++ src/main/java/it/contrader/service/StudentService.java
@@ -0,0 +1,22 @@
+package it.contrader.service;
+
+import java.util.List;
+
+import it.contrader.dao.StudentDAO;
+import it.contrader.model.Student;
+
+public class StudentService {
+	
+	private StudentDAO studentDAO;
+	
+	public StudentService() {
+		this.studentDAO = new StudentDAO();
+	}
+	
+	public List<Student> getAll() {
+		return this.studentDAO.getAll();
+	}
+	public Student read (int id) {
+		return this.studentDAO.read(id);
+	}
+	
