93f1dfc8988afbe91f41e7df95272c14edc4c156
Emilia <emiliaesposito-ee@libero.it>
2019-09-03 12:50:24 +0200

    TrainingService

---
+++ src/main/java/it/contrader/service/TrainingService.java
@@ -0,0 +1,30 @@
+package it.contrader.service;
+
+import java.util.List;
+
+import it.contrader.dao.TrainingDAO;
+import it.contrader.model.Training;
+
+public class TrainingService {
+	
+	private TrainingDAO trainingDAO;
+	
+	public TrainingService( ) {
+		this.trainingDAO = new TrainingDAO();
+	}
+	public List<Training> getAll();{
+		return this.trainingDAO.getAll():
+	}
+	public Training read (int id) {
+		return this.trainingDAO.read(id);
+	}
+	public boolean delete (int trainingId ) {
+		return trainingDAO.delete(trainingId);
+	}
+	public boolean insert (Training training ) {
+		return trainingDAO.insert(training);
+	}
+	public boolean update (Training training) {
+		return trainingDAO.update(training);
+	}
+}
