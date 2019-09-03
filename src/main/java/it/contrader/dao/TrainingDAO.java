97bf9b719e891f7865d5bbca0e3751db2c9ce043
marcoleka <marcoleka19@gmail.com>
2019-09-03 12:19:53 +0200

    trainingDAO

--- src/main/java/it/contrader/dao/TrainingDAO.java
+++ src/main/java/it/contrader/dao/TrainingDAO.java
@@ -36,7 +36,7 @@ public class TrainingDAO {
 				int idStudent = resultSet.getInt("idStudent");
 				training = new Training(nameTraining, idGroup, idStudent);
 				training.setId(id);
-				trainingList.add(training)
+				trainingList.add(training);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -49,8 +49,86 @@ public class TrainingDAO {
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_CREATE);
 			preparedStatement.setString(1, trainingToInsert.getNameTraining());
-			
+			preparedStatement.setInt(2, trainingToInsert.getIdGroup());
+			preparedStatement.setInt(3, trainingToInsert.getIdStudent());
+			return true;
+		} catch (SQLException e) {
+			return false;
 		}
+	}
+	
+	public Training read(int trainingId) {
+		Connection connection = ConnectionSingleton.getInstance();
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_READ);
+			preparedStatement.setInt(1, trainingId);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet.next();
+			String nameTraining;
+			int idGroup, idStudent;
+			nameTraining = resultSet.getString("nameTraining");
+			idGroup = resultSet.getInt("idGroup");
+			idStudent = resultSet.getInt("idStudent");
+			Training training = new Training(nameTraining, idGroup, idStudent);
+			training.setId(resultSet.getInt("id"));
+			return training;			
+		} catch (SQLException e) {
+			return null;
+		}
+		
+	}
+	
+	public boolean update(Training trainingToUpdate) {
+		Connection connection = ConnectionSingleton.getInstance();
+		//Check id
+		if (trainingToUpdate.getId() == 0)
+			return false;
+			
+			Training trainingRead = read(trainingToUpdate.getId());
+			if(!trainingRead.equals(trainingToUpdate)) {
+				try {
+					if(trainingToUpdate.getNameTraining() == null || trainingToUpdate.getNameTraining().equals("")) {
+						trainingToUpdate.setNameTraining(trainingRead.getNameTraining());
+					}
+					
+					if(trainingToUpdate.getIdGroup() == 0) {
+						trainingToUpdate.setIdGroup(trainingRead.getIdGroup());
+					}
+					
+					if(trainingToUpdate.getIdStudent() == 0) {
+						trainingToUpdate.setIdStudent(trainingRead.getIdStudent());
+					}
+					
+					//Update Training
+					PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(QUERY_UPDATE);
+					preparedStatement.setString(1, trainingToUpdate.getNameTraining());
+					preparedStatement.setInt(2, trainingToUpdate.getIdGroup());
+					preparedStatement.setInt(3, trainingToUpdate.getIdStudent());
+					int a = preparedStatement.executeUpdate();
+					if(a > 0)
+						return true;
+					else
+						return false;
+					
+				} catch (SQLException e) {
+					return false;
+				}
+			}
+			
+			return false;
+	}
+	
+	public boolean delete (int id) {
+		Connection connection = ConnectionSingleton.getInstance();
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_DELETE);
+			preparedStatement.setInt(1, id);
+			int n = preparedStatement.executeUpdate();
+			if (n != 0)
+				return true;
+			} catch (SQLException e) {
+			}
+			return false;
 	}
 
 }
