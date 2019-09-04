0f16b1105156b8c927851079361cb1b6f9af6ee3
pdiaferio91 <p.diaferio2@studenti.uniba.it>
2019-09-04 10:20:03 +0200

    ContentController-commit

--- src/main/java/it/contrader/controller/ContentController.java
+++ src/main/java/it/contrader/controller/ContentController.java
@@ -1,5 +1,7 @@
 package it.contrader.controller;
 
+import java.util.List;
+
 import it.contrader.main.MainDispatcher;
 import it.contrader.model.Content;
 import it.contrader.service.ContentService;
@@ -17,7 +19,7 @@ public class ContentController implements Controller {
 	public void doControl(Request request) {
 		//Estrae mode choice
 		String mode = (String) request.get("mode");
-		String choise = (String) request.get("choise");
+		String choice = (String) request.get("choise");
 		
 		int id;
 		String text, title, tag;
@@ -34,10 +36,80 @@ public class ContentController implements Controller {
 			
 		case "INSERT":
 			title = request.get("title").toString();
+			text = request.get("text").toString();
+			tag = request.get("tag").toString();
 			idStudent = Integer.parseInt(request.get("idStudent").toString());
 			
-		}
-	}
-	
+			Content contentinsert = new Content(title, text, tag, idStudent);
+			contentService.insert(contentinsert);
+			request = new Request();
+			request.put("mode", "mode");
+			MainDispatcher.getInstance().callView(sub_package + "ContentInsert", request);
+			break;
+			
+		case "DELETE":
+			id = Integer.parseInt(request.get("id").toString());
+			contentService.delete(id);
+			request = new Request();
+			request.put("mode", "mode");
+			MainDispatcher.getInstance().callView(sub_package + "ContentDelete", request);
+			break;
+			
+		case "UPDATE" :
+			id = Integer.parseInt(request.get("id").toString());
+			title = request.get("title").toString();
+			text = request.get("text").toString();
+			tag = request.get("tag").toString();
+			idStudent = Integer.parseInt(request.get("idStudent").toString());
+			Content contenttoupdate = new Content(title, text, tag, idStudent);
+			contenttoupdate.setid(id);
+			contentService.update(contenttoupdate);
+			request = new Request();
+			request.put("mode", "mode");
+			MainDispatcher.getInstance().callView(sub_package + "ContentUpdate", request);
+			break;
+			
+		case "CONTENTLIST" :
+			List<Content> contents = contentService.getAll();
+			request.put("contents", contents);
+			MainDispatcher.getInstance().callView("Content", request);
+			break;
+			
+		case "GETCHOICE" :
+			switch (choice.toUpperCase()) {
+			
+			case "L":
+				MainDispatcher.getInstance().callView(sub_package + "ContentRead", null);
+				break;
+				
+			case "I":
+				MainDispatcher.getInstance().callView(sub_package + "ContentInsert", null);
+				break;
+				
+			case "M":
+				MainDispatcher.getInstance().callView(sub_package + "ContentUpdate", null);
+				break;
+				
+			case "C":
+				MainDispatcher.getInstance().callView(sub_package + "ContentDelete", null);
+				break;
+				
+			case "E":
+				MainDispatcher.getInstance().callView("Login", null);
+				break;
 
+			case "B":
+				MainDispatcher.getInstance().callView("HomeAdmin", null);
+				break;
+				
+			default:
+				MainDispatcher.getInstance().callView("Login", null);
+			}
+			
+			default:
+				MainDispatcher.getInstance().callView("Login", null);
+			
+			}
+		}
 }
+	
