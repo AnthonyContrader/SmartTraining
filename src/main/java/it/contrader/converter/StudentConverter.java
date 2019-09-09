68790a48ea749437f52db489e581f52a5fb1cfaa
davidecherubini <davidetitocherubini@gmail.com>
2019-09-09 16:17:52 +0200

    StudentConverter

--- src/main/java/it/contrader/converter/StudentConverter.java
+++ src/main/java/it/contrader/converter/StudentConverter.java
@@ -1,5 +1,36 @@
 package it.contrader.converter;
 
-public class StudentConverter {
+import java.util.ArrayList;
+import java.util.List;
 
+import it.contrader.dto.StudentDTO;
+import it.contrader.model.Student;
+
+public class StudentConverter implements Converter<Student, StudentDTO> {
+	public StudentDTO toDTO(Student student) {
+		StudentDTO studentdto = new StudentDTO(student.getId(), studentDTO.getName(), StudentDTO.getSurname(), studentDTO.getidUser());
+		return studentDTO;
+	}
+	
+	public Student toEntety(StudentDTO studentDTO) {
+		Student student = new Student(studentDTO.getId(), studentDTO.getName(), studentDTO.getSurname(), studentDTO.getIdUser());
+		return student;
+	}
+	
+	public List<StudentDTO> toDTOList(List<Student> studentList){
+		List<StudentDTO> studentDTOList = new ArrayList<StudentDTO>();
+		
+		for(Student student : studentList) {
+			studentDTOList.add(toDTO(student));
+		}
+		return studentDTOList;
+	}
+
+	@Override
+	public Student toEntity(StudentDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+   
 }
+
