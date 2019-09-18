c605634b78884abed41a859f74ba2026c76c39d8
MatteoS7 <matteo.salemi@gmail.com>
2019-09-18 11:28:38 +0200

    sistemata read training

--- src/main/java/it/contrader/controller/TrainingController.java
+++ src/main/java/it/contrader/controller/TrainingController.java
@@ -65,7 +65,7 @@ public class TrainingController {
 	@GetMapping("/read")
 	public String read(HttpServletRequest request, @RequestParam("id") Long id) {
 		request.getSession().setAttribute("dto", service.read(id));
-		return "training/training";
+		return "training/readtraining";
 	}
 
 	@GetMapping("/logout")
