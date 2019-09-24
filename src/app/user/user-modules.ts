3014ca7d3877fc6aa6680ca200f469d39c48a45d
pdiaferio91 <p.diaferio2@studenti.uniba.it>
2019-09-24 12:05:32 +0200

    user

---
+++ src/app/user/user-modules.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserRoutingModule } from './user-routing.module';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AccountComponent } from './account/account.component';
+
+
+/**
+ * Modulo dell'admin, qui vengono dichiarate le component che utilizza 
+ * l'admin. Questo modulo importa AdminRoutingModule.
+ * 
+ * @author Vittorio Valent
+ * 
+ * @see AdminRoutingModule
+ */
+@NgModule({
+  declarations: [UserDashboardComponent,AccountComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    FormsModule
+  ]
+})
+export class UserModule { }
