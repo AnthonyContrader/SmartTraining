ff7bd5bf3619104512af756b9f62c6f2245a02a9
marcoleka <marcoleka19@gmail.com>
2019-09-26 15:06:46 +0200

    gruppo

--- src/main/java/it/contrader/controller/GruppoController.java
+++ src/main/java/it/contrader/controller/GruppoController.java
@@ -1,5 +1,19 @@
 package it.contrader.controller;
 
-public class GruppoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.contrader.dto.GruppoDTO;
+import it.contrader.service.GruppoService;
+
+@RestController
+@RequestMapping("/gruppo")
+@CrossOrigin()
+public class GruppoController extends AbstractController<GruppoDTO>{
+	
+	@Autowired
+	private GruppoService gruppoService;
 
 }
