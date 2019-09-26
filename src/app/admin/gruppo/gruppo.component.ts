710a897eb2b73cb4a857bfcbb5710f697a8eb1a2
Emilia <emiliaesposito-ee@libero.it>
2019-09-26 12:46:32 +0200

    front gruppo

--- src/app/admin/gruppo/gruppo.component.ts
+++ src/app/admin/gruppo/gruppo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/service/student.service';
+import { GruppoService } from 'src/service/gruppo.service';
+import { StudentDTO } from 'src/dto/studentdto';
+import { GruppoDTO } from 'src/dto/gruppodto';
+import { AbstractCrudComponent } from 'src/app/utils/abstractcomponent';
 
 @Component({
   selector: 'app-gruppo',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gruppo.component.css']
 })
 export class GruppoComponent implements OnInit {
+ 
+  gruppi: GruppoDTO[];
+  gruppotoinsert: GruppoDTO = new GruppoDTO();
+  students: StudentDTO[];
 
-  constructor() { }
+  constructor(private service: GruppoService, private sService: StudentService) { }
 
   ngOnInit() {
+    this.getGruppo();
+    this.getStudent();
+  }
+
+  getGruppo(){
+    this.service.getAll().subscribe(gruppi=>this.gruppi=gruppi);
+  }
+
+  getStudent(){
+    this.sService.getAll().subscribe(students=> this.students=students);
+  }
+
+  delete(gruppo:GruppoDTO){
+    this.service.delete(gruppo.id).subscribe(()=> this.getGruppo());
+  }
+
+  update(gruppo:GruppoDTO){
+    this.service.update(gruppo).subscribe(()=> this.getGruppo());
+  }
+
+  insert(gruppo:GruppoDTO){
+    this.service.insert(gruppo).subscribe(()=> this.getGruppo());
+  }
+
+  clear(){
+    this.gruppotoinsert = new GruppoDTO();
   }
 
 }
