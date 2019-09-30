976129cceb2bd808bb0d553227840e632ba4f4d1
marcoleka <marcoleka19@gmail.com>
2019-09-30 15:52:28 +0200

    Initial version of gateway generated by JHipster-6.3.1

---
+++ gateway/src/main/java/it/contrader/service/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package it.contrader.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
